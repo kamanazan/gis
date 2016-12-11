bf8ece72a945a78840597fa7db042ab5f75c8303
Muhammad Fauzan Rosyadi <fauzan.rosyadi@gmail.com>
2016-12-12 05:50:04 +0700

    pake ol-extended untuk control layer

--- gis_rauf/bandungis/static/bandungis/js/bandungis.js
+++ gis_rauf/bandungis/static/bandungis/js/bandungis.js
@@ -1,72 +1,337 @@
 // Pastikan file ini terletak setelah <div id="map" class="map"></div>
-var style = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 255, 255, 0.6)'
-    }),
-    stroke: new ol.style.Stroke({
-        color: '#319FD3',
-        width: 1
-    }),
-    text: new ol.style.Text({
-        font: '12px Calibri,sans-serif',
-        fill: new ol.style.Fill({
-            color: '#000'
-        }),
-        stroke: new ol.style.Stroke({
-            color: '#fff',
-            width: 3
-        })
-    })
-});
-
-var style2 = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 255, 255, 0.6)'
-    }),
-    stroke: new ol.style.Stroke({
-        color: '# #DAF7A6 ',
-        width: 1
-    }),
-    text: new ol.style.Text({
-        font: '12px Calibri,sans-serif',
-        fill: new ol.style.Fill({
-            color: '#000'
-        }),
-        stroke: new ol.style.Stroke({
-            color: '#fff',
-            width: 3
-        })
-    })
-});
-
-var kabupatenLayer = new ol.layer.Vector({
+// Layer untuk peta dasar
+var kabupatenLayer = new ol.layer.Image({
     title: 'Kabupaten',
-    source: new ol.source.Vector({
-        url: '/static/bandungis/data/kabupaten.geojson',
-        format: new ol.format.GeoJSON(),
-    }),
-    style: function(feature, resolution) {
-        style.getText().setText(resolution < 128 ? feature.get('name') : '');
-        return style;
-    }
+    source:  new ol.source.ImageWMS({
+            url: 'http://127.0.0.1:8080/geoserver/wms',
+            params: {
+                'LAYERS': 'SIMTARU:kabupaten',
+                'FORMAT': 'image/png'
+            },
+            serverType: 'geoserver'
+    })
 });
 
-var kecamatanLayer = new ol.layer.Vector({
+var kabupatenBatasLayer = new ol.layer.Image({
+    title: 'Batas Kabupaten',
+    source:  new ol.source.ImageWMS({
+            url: 'http://127.0.0.1:8080/geoserver/wms',
+            params: {
+                'LAYERS': 'SIMTARU:kabupaten_batas',
+                'FORMAT': 'image/png'
+            },
+            serverType: 'geoserver'
+    })
+});
+
+var kecamatanLayer = new ol.layer.Image({
     title: 'Kecamatan',
-    source: new ol.source.Vector({
-        url: '/static/bandungis/data/kecamatan.geojson',
-        format: new ol.format.GeoJSON(),
+    source:  new ol.source.ImageWMS({
+            url: 'http://127.0.0.1:8080/geoserver/wms',
+            params: {
+                'LAYERS': 'SIMTARU:kecamatan',
+                'FORMAT': 'image/png'
+            },
+            serverType: 'geoserver'
+    })
+});
 
+var kecamatanBatasLayer = new ol.layer.Image({
+    title: 'Batas Kecamatan',
+    source:  new ol.source.ImageWMS({
+            url: 'http://127.0.0.1:8080/geoserver/wms',
+            params: {
+                'LAYERS': 'SIMTARU:kecamatan_batas',
+                'FORMAT': 'image/png'
+            },
+            serverType: 'geoserver'
+    })
+});
+
+var desaLayer = new ol.layer.Image({
+    title: 'Desa',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:desa',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var desaBatasLayer = new ol.layer.Image({
+    title: 'Batas Desa',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:desa_batas',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var jalanLayer = new ol.layer.Image({
+    title: 'Jalan',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:jalan',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var provinsiLayer = new ol.layer.Image({
+    title: 'Provinsi',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:provinsi',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var sungaiLayer = new ol.layer.Image({
+    title: 'Sungai',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:sungai',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var pemerintahanLayer = new ol.layer.Image({
+    title: 'Pusat Pemerintahan',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pemerintahan',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+//Layer untuk pola ruang
+
+var polaAreaPariwisataLayer = new ol.layer.Image({
+    title: 'Area Pariwisata',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_area_pariwisata',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaSungaiLayer = new ol.layer.Image({
+    title: 'Sungai',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_aliran_sungai',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaDesaLayer = new ol.layer.Image({
+    title: 'Desa',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_desa',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaDesaBatasLayer = new ol.layer.Image({
+    title: 'Batas Desa',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_desa_batas',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaEnergiIndukLayer = new ol.layer.Image({
+    title: 'Energi Induk',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_energi_induk',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaJalanLayer = new ol.layer.Image({
+    title: 'Jalan',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_jalan',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaKabupatenLayer = new ol.layer.Image({
+    title: 'Kabupaten',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_kabupaten',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaKabupatenBatasLayer = new ol.layer.Image({
+    title: 'Batas Kabupaten',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_kabupaten_batas',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaKecamatanLayer = new ol.layer.Image({
+    title: 'Kecamatan',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_kecamatan',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaKecamatanBatasLayer = new ol.layer.Image({
+    title: 'Batas Kecamatan',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_kecamatan_batas',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaKeretaJalurLayer = new ol.layer.Image({
+    title: 'Jalur Kereta Api',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_kereta_jalur',
+            'FORMAT': 'image/png'
+        },
+        serverType: 'geoserver'
     }),
-    style: function(feature, resolution) {
-          style2.getText().setText(resolution < 128 ? feature.get('name') : '');
-          return style2;
-    }
+    serverType: 'geoserver'
 });
-var overlayGroup = new ol.layer.Group({
-    title: 'Overlays',
-    layers: [kabupatenLayer, kecamatanLayer]
+
+var polaPariwisataLayer = new ol.layer.Image({
+    title: 'Pariwisata',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMATARU:pola_pariwisata',
+            'FORMAT': 'img/png'
+        },
+        serverType: 'geoserver'
+    })
 });
+
+var polaProvinsiLayer = new ol.layer.Image({
+    title: 'Provinsi',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_provinsi',
+            'FORMAT': 'img/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaPemerintahanLayer = new ol.layer.Image({
+    title: 'Pemerintahan',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_pemerintahan',
+            'FORMAT': 'img/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaRuangLayer = new ol.layer.Image({
+    title: 'Ruang',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_ruang',
+            'FORMAT': 'img/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+
+var polaTambangLayer = new ol.layer.Image({
+    title: 'Tambang',
+    source: new ol.source.ImageWMS({
+        url: 'http://127.0.0.1:8080/geoserver/wms',
+        params: {
+            'LAYERS': 'SIMTARU:pola_tambang',
+            'FORMAT': 'img/png'
+        },
+        serverType: 'geoserver'
+    })
+});
+// Deklarasi layer group untuk peta dasar
+
+var petaDasar = new ol.layer.Group({
+    title: 'Peta Administratif',
+    openInLayerSwitcher: true,
+    layers: [kabupatenLayer, kabupatenBatasLayer, kecamatanLayer, kecamatanBatasLayer, desaLayer, desaBatasLayer, provinsiLayer, pemerintahanLayer, jalanLayer, sungaiLayer]
+});
+
+var petaPola = new ol.layer.Group({
+    title: 'Peta Rancang Pola',
+    openInLayerSwitcher: true,
+    layers: [polaProvinsiLayer, polaRuangLayer, polaDesaLayer, polaDesaBatasLayer, polaKecamatanLayer,
+    polaKecamatanBatasLayer, polaKabupatenLayer, polaKabupatenBatasLayer, polaSungaiLayer, polaJalanLayer,
+    polaTambangLayer, polaEnergiIndukLayer, polaAreaPariwisataLayer, polaPemerintahanLayer, polaKeretaJalurLayer,
+    polaPariwisataLayer,
+    ]
+})
+
 var map = new ol.Map({
     layers: [
         new ol.layer.Group({
@@ -81,14 +346,13 @@ var map = new ol.Map({
                 }),
             ]
         }),
-        overlayGroup
+        petaDasar,
+        petaPola
     ],
     target: 'map',
-    controls: ol.control.defaults({
-      attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-        collapsible: false
-      })
-    }),
+    controls: ol.control.defaults().extend([// Add a new Layerswitcher to the map
+        new ol.control.LayerSwitcher()
+    ]),
     view: new ol.View({
       center: ol.proj.transform([107.5853139, -6.9840138], 'EPSG:4326', 'EPSG:3857'),
       zoom: 11,
@@ -96,78 +360,18 @@ var map = new ol.Map({
       maxZoom: 13
     })
 });
-// Layer switcher control
-var layerSwitcher = new ol.control.LayerSwitcher();
-map.addControl(layerSwitcher);
-//overlayGroup.getLayers().push(new ol.layer.Tile({title: 'Kabupaten', source:kabupatenLayer}));
-//overlayGroup.getLayers().push(new ol.layer.Tile({title: 'Kecamatan', source:kecamatanLayer}));
-// animasi ketika hover
-var highlightStyleCache = {};
-var featureOverlay = new ol.layer.Vector({
-source: new ol.source.Vector(),
-map: map,
-style: function(feature, resolution) {
-  var text = resolution < 5000 ? feature.get('name') : '';
-  if (!highlightStyleCache[text]) {
-    highlightStyleCache[text] = new ol.style.Style({
-      stroke: new ol.style.Stroke({
-        color: '#f00',
-        width: 1
-      }),
-      fill: new ol.style.Fill({
-        color: 'rgba(255,0,0,0.1)'
-      }),
-      text: new ol.style.Text({
-        font: '12px Calibri,sans-serif',
-        text: text,
-        fill: new ol.style.Fill({
-          color: '#000'
-        }),
-        stroke: new ol.style.Stroke({
-          color: '#f00',
-          width: 3
-        })
-      })
-    });
-  }
-  return highlightStyleCache[text];
-}
+// Add a layer switcher outside the map
+var switcher = new ol.control.LayerSwitcher({
+    target:$(".layerSwitcher").get(0),
+    show_progress:true,
+    extent: true,
+    trash: true,
+    oninfo: function (l) { alert(l.get("title")); }
 });
 
-var highlight;
-var displayFeatureInfo = function(pixel) {
-
-var feature = map.forEachFeatureAtPixel(pixel, function(feature) {
-  return feature;
-});
-
-var info = document.getElementById('info');
-if (feature) {
-  info.innerHTML = feature.getId() + ': ' + feature.get('name');
-} else {
-  info.innerHTML = '&nbsp;';
+map.addControl(switcher);
+// Insert mapbox layer in layer switcher
+function displayInLayerSwitcher(b)
+{
+    mapbox.set('displayInLayerSwitcher', b);
 }
-
-if (feature !== highlight) {
-  if (highlight) {
-    featureOverlay.getSource().removeFeature(highlight);
-  }
-  if (feature) {
-    featureOverlay.getSource().addFeature(feature);
-  }
-  highlight = feature;
-}
-
-};
-
-map.on('pointermove', function(evt) {
-if (evt.dragging) {
-  return;
-}
-var pixel = map.getEventPixel(evt.originalEvent);
-displayFeatureInfo(pixel);
-});
-
-map.on('click', function(evt) {
-displayFeatureInfo(evt.pixel);
-});
